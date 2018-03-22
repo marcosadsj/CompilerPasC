4756144919fdc62e394a1159e7da5a5662be1b61
marcosadsj <marcosadsj@gmail.com>
2018-03-22 11:56:37 -0300

    removendo conteúdo desnecessário

--- src/lexer/FileHelper.java
+++ src/lexer/FileHelper.java
@@ -6,47 +6,5 @@ import java.io.IOException;
 
 public class FileHelper {
 	
-	private int fallbackPosition;
-	private int currentPosition;
 	
-	private BufferedReader bufferedReader;
-	
-	private static StringBuilder stringBuilder;
-	
-	
-	
-	public FileHelper(String path) {
-		
-		stringBuilder = new StringBuilder();
-		
-		try {
-			bufferedReader = new BufferedReader(new FileReader(path));
-		
-			fallbackPosition++;
-			currentPosition++;
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			while(bufferedReader.ready()) {
-				stringBuilder.append(bufferedReader.readLine());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-		
-	public int getFallbackPosition() {
-		return fallbackPosition;
-	}
-
-	public int getCurrentPosition() {
-		return currentPosition;
-	}
-
-	public static StringBuilder getContent() {
-		return stringBuilder;
-	}
 }
--- src/resources/Starter.java
+++ src/resources/Starter.java
@@ -1,36 +1,8 @@
 package resources;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Starter {
 	
-	public static void main(String args[]) throws IOException{
-		
-		File file = null;
-		BufferedReader bufferedReader = null;
-		FileReader fileReader = null;
-		
-		file = new File("/home/marcosadsj/Eclipse-Workspace/CompilerPasC/src/file");
-		
-		try {
-			fileReader = new FileReader(file);
-			bufferedReader = new BufferedReader(fileReader);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	
-		
-		while(bufferedReader.ready()) {
-			try {
-				String text = bufferedReader.readLine();
-				System.out.println(text);
-			} catch (IOException e) {
-								e.printStackTrace();
-			}
-		}
+	public static void main(String args[]){
 		
 	}
 }
