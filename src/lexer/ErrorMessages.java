2e8bcbab2cade9062069e6ee55191b5b3a1c1371
marcosadsj <marcosadsj@gmail.com>
2018-03-22 12:48:54 -0300

    Classe de erros

---
+++ src/lexer/ErrorMessages.java
@@ -0,0 +1,28 @@
+package lexer;
+
+public class ErrorMessages {
+	
+	private ErrorMessages() {}
+	
+	
+	private static final String RED_COLOR ="\n" + (char)27+ "[31m";
+	private static final String ERROR_TYPE = "[Lexical Error]:";
+	
+	private static final String ERROR_CONFIG = RED_COLOR + ERROR_TYPE;
+	
+	public static void invalidCaractere(char catactere, int line, int column){
+		System.out.println(ERROR_CONFIG + "Caractere invalido " + catactere + " na linha " + line + " e coluna " + column);
+	}
+	
+	public static void incompleteToken(int line, int column){
+		System.out.println(ERROR_CONFIG + "Token incompleto para o caractere ! na linha " + line + " e coluna " + column);
+	}
+	
+	public static void unclosedString(){
+		System.out.println(ERROR_CONFIG + "String deve ser fechada com \" antes do fim de arquivo");
+	}
+	
+	public static void invalidDoublePattern(int line, int column){
+		System.out.println(ERROR_CONFIG + "Padrão para double inválido na linha " + line + " coluna " + column);
+	}
+}
