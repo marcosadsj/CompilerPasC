581eea590f9453df09a7982bd08bd82ffbc3f2d0
marcosadsj <marcosadsj@gmail.com>
2018-03-22 11:49:49 -0300

    adicionado get e seter e override toString do symbolsTabke

--- src/resources/SymbolsTable.java
+++ src/resources/SymbolsTable.java
@@ -6,24 +6,54 @@ import lexer.Token;
 
 public class SymbolsTable {
 
-	private HashMap<Token,Description> sysmbolsTable;
+	private HashMap<Token,Description> symbolsTable;
 	
 	public SymbolsTable() {
 		
-		sysmbolsTable = new HashMap<>();
+		symbolsTable = new HashMap<>();
 		
-		sysmbolsTable.put(new Token(Tags.KW,"program"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"if"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"else"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"while"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"write"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"read"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"num"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"char"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"not"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"or"), new Description());
-		sysmbolsTable.put(new Token(Tags.KW,"and"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"program"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"if"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"else"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"while"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"write"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"read"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"num"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"char"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"not"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"or"), new Description());
+		symbolsTable.put(new Token(Tags.KW,"and"), new Description());
 
-
+	}
+	
+	public void setSymbol(Token token, Description description) {
+		symbolsTable.put(token, description);
+	}
+	
+	public Token getToken(String lexema) {
+		for(Token token: symbolsTable.keySet()) {
+			if(token.getLexeme().equalsIgnoreCase(lexema)) {
+				return token;
+			}
+		}
+		return null;
+	}
+	
+	public Description getDescription(Token token){
+		return (Description) symbolsTable.get(token);
+	}
+	
+	@Override
+	public String toString() {
+		
+		int i = 0;
+		
+		String output = "";
+		
+		for(Token token : symbolsTable.keySet()) {
+			output += "position " + i + ": \t" + token.toString() + "\n";
+			i++;
+		}
+		return output;
 	}
 }
