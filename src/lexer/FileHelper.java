400b4979b477802ca6dc3c0f04587484f3101e56
marcosadsj <marcosadsj@gmail.com>
2018-03-22 13:36:49 -0300

    abrir e fechar arquivo

--- src/lexer/FileHelper.java
+++ src/lexer/FileHelper.java
@@ -1,10 +1,36 @@
 package lexer;
-import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class FileHelper {
 	
+	private RandomAccessFile acessFile;
 	
+	public FileHelper(String path) {
+		try {
+			acessFile = new RandomAccessFile(path,"r");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+	}
+	
+	public void closeFile() {
+		if(acessFile != null) {
+			try {
+				acessFile.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(3);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(4);
+			}
+		}
+	}
 }
