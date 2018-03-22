b4dac70dabeb723a910fb8e1f6304fc7d2accc87
marcosadsj <marcosadsj@gmail.com>
2018-03-22 14:23:10 -0300

    mudado package

--- src/lexer/Starter.java
+++ src/lexer/Starter.java
@@ -1,4 +1,4 @@
-package resources;
+package lexer;
 
 public class Starter {
 	
---
+++ src/lexer/SymbolsTable.java
@@ -0,0 +1,50 @@
+package lexer;
+import java.util.HashMap;
+
+import resources.KeyWords;
+import resources.Tags;
+
+public class SymbolsTable {
+
+	private HashMap<Token,Description> symbolsTable;
+	
+	public SymbolsTable() {
+		
+		symbolsTable = new HashMap<>();
+		
+		for(String keyWord : KeyWords.getAllKeyWords()) {
+			symbolsTable.put(new Token(Tags.KW,keyWord), new Description());
+		}
+	}
+	
+	public void setSymbol(Token token, Description description) {
+		symbolsTable.put(token, description);
+	}
+	
+	public Token getToken(String lexema) {
+		for(Token token: symbolsTable.keySet()) {
+			if(token.getLexeme().equalsIgnoreCase(lexema)) {
+				return token;
+			}
+		}
+		return null;
+	}
+	
+	public Description getDescription(Token token){
+		return symbolsTable.get(token);
+	}
+	
+	@Override
+	public String toString() {
+		
+		int i = 0;
+		
+		String output = "";
+		
+		for(Token token : symbolsTable.keySet()) {
+			output += "position " + i + ": \t" + token.toString() + "\n";
+			i++;
+		}
+		return output;
+	}
+}
